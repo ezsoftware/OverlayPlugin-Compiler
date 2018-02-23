3c23c2868a199cdabb4dd827d62f7f6c484650d1
ezsoftware <john@ezsoftwareinc.com>
2018-02-23 11:11:21 -0700

    update to cactbot 0.6.3

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @SETLOCAL
 @ECHO off
-SET VERSION=0.3.4.0.EZS.Enmity-1.9.3.0.Cactbot-0.6.2
+SET VERSION=0.3.4.0.EZS.Enmity-1.9.3.0.Cactbot-0.6.3
 ECHO "Removing Old Build..."
 CALL CLEAN
 
--- createInstaller.bat
+++ createInstaller.bat
@@ -1,3 +1,3 @@
 @echo off 
-SET VERSION=0.3.4.0.EZS.Enmity-1.9.3.0.Cactbot-0.6.2 
-call "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" F:\Repositories\OverlayPlugin-Compiler\installer.iss /dMyAppVersion=0.3.4.0.EZS.Enmity-1.9.3.0.Cactbot-0.6.2 
+SET VERSION=0.3.4.0.EZS.Enmity-1.9.3.0.Cactbot-0.6.3 
+call "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" F:\Repositories\OverlayPlugin-Compiler\installer.iss /dMyAppVersion=0.3.4.0.EZS.Enmity-1.9.3.0.Cactbot-0.6.3 
